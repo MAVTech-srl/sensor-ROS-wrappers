a3f1a8b6f54f3ba41d25f770fde762c72e605919
DavideCarminati <davide.carminati@outlook.com>
2025-02-21 12:06:06 +0100

    Initial commit. Tested for all three sensors

---
+++ livox_AVIA/ros_entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# add sourcing to .bashrc
+echo "export ROS_DISTRO=humble" >> ~/.bashrc
+echo "source /opt/ros/\$ROS_DISTRO/setup.bash" >> ~/.bashrc
+echo "source ~/ros2_ws/install/local_setup.bash" >> ~/.bashrc
+
+# setup ros2 environment
+export ROS_DISTRO=humble
+source /opt/ros/"$ROS_DISTRO"/setup.bash --
+source ~/ros2_ws/install/local_setup.bash --
+
+ros2 launch livox_ros2_driver livox_lidar_msg_launch.py
+
+exec "$@"
---
+++ livox_MID360/start_docker_livox.sh
@@ -0,0 +1,35 @@
+#sudo docker run --runtime nvidia -it --rm --network host -e DISPLAY=$DISPLAY \
+#     --device /dev/video0 --device /dev/video1 --device /dev/video2 --device /dev/video4 \
+#     --device /dev/video5 \
+#     -v /tmp/.X11-unix/:/tmp/.X11-unix \
+#     -v $XAUTH:$XAUTH -e XAUTHORITY=$XAUTH \
+#     dustynv/realsense:r36.2.0
+     
+
+DOCKER_ARGS+=("-v /tmp/.X11-unix:/tmp/.X11-unix")
+DOCKER_ARGS+=("-v $HOME/.Xauthority:/home/rs/.Xauthority:rw")
+DOCKER_ARGS+=("-e DISPLAY")
+DOCKER_ARGS+=("-e NVIDIA_VISIBLE_DEVICES=all")
+DOCKER_ARGS+=("-e NVIDIA_DRIVER_CAPABILITIES=all")
+DOCKER_ARGS+=("--user 1000")
+#DOCKER_ARGS+=("-e USER")
+#DOCKER_ARGS+=("-e HOST_USER_UID=`id -u`")
+#DOCKER_ARGS+=("-e HOST_USER_GID=`id -g`")
+
+DOCKER_ARGS+=("-e NVIDIA_VISIBLE_DEVICES=nvidia.com/gpu=all,nvidia.com/pva=all")
+DOCKER_ARGS+=("-v /usr/bin/tegrastats:/usr/bin/tegrastats")
+DOCKER_ARGS+=("-v /tmp/:/tmp/")
+DOCKER_ARGS+=("-v /usr/lib/aarch64-linux-gnu/tegra:/usr/lib/aarch64-linux-gnu/tegra")
+DOCKER_ARGS+=("-v /usr/src/jetson_multimedia_api:/usr/src/jetson_multimedia_api")
+DOCKER_ARGS+=("--pid=host")
+DOCKER_ARGS+=("-v /usr/share/vpi3:/usr/share/vpi3")
+DOCKER_ARGS+=("-v /dev/input:/dev/input")    
+    
+docker run -it --rm \
+    --privileged \
+    --network host \
+    --ipc=host \
+    ${DOCKER_ARGS[@]} \
+    --runtime nvidia \
+    prova/livox
+
